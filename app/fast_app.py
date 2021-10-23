69a8c2fd69eb25916006f0b39ef9ae30bde7b169
Rahul Pandey <rpandey1901@gmail.com>
2021-10-23 21:46:01 +0200

    Working version of docker container

--- app/fast_app.py
+++ app/fast_app.py
@@ -36,5 +36,7 @@ def predict_species(inputdict: Modeldict):
         'prediction': prediction
     }
 
-if __name__ == '__main__':
-  uvicorn.run(app,host="127.0.0.1",port=9696)
+#if __name__ == '__main__':
+  #uvicorn.run(app,host="127.0.0.1",port=9696)
+  #uvicorn.run(app, host='0.0.0.0', port=8002)
+  #uvicorn.run(app, host='0.0.0.0')
